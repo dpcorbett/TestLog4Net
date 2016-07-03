b7badda8f148e15301f85e4b47f13f3da4183750
dpcorbett <dpcomputing@gmail.com>
2016-07-03 21:11:27 +0930

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace TestLog4Net
+{
+    class Program
+    {
+        private static readonly log4net.ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            _log.Error("This is my error message.");
+            Console.ReadLine();
+        }
+    }
+}
