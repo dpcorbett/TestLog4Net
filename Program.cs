da87d490e2597451a227883046e302498c4e0e12
dpcorbett <dpcomputing@gmail.com>
2016-07-04 21:51:31 +0930

    Move GetLogger to external library.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+﻿using SharpTools.Log;
+using System;
 
 namespace TestLog4Net
 {
     class Program
     {
         //private static readonly log4net.ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        private static readonly log4net.ILog _log = LogHelper.GetLogger();
+        private static readonly log4net.ILog _log = SharpToolsLog.GetLogger();
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            _log.Debug("Developer: Tutorial was run");
+            _log.Info("Maintenance: water pump turned on");
+            _log.Warn("Maintenance: the water pump is getting hot");
 
-            _log.Error("This is my error message.");
+            var i = 0;
+
+            try
+            {
+                var x = 10 / i;
+            }
+            catch (DivideByZeroException exc)
+            {
+                _log.Error("Developer: we tried to divide by zero again", exc);
+            }
+
+            _log.Fatal("Maintenance: water pump exploded");
+
             Console.ReadLine();
         }
     }
